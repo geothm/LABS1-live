c5918996ea2c834a8e88d4d1368eaa7a45c6e03b
Georgia Papp <georgia.papp@irian.ro>
2023-05-15 17:16:34 +0300

    entities working

--- pizza-app/pizza-domain/src/main/java/ro/irian/pizzaapp/domain/Ingredient.java
+++ pizza-app/pizza-domain/src/main/java/ro/irian/pizzaapp/domain/Ingredient.java
@@ -1,12 +1,31 @@
 package ro.irian.pizzaapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Ingredient {
 
+    @Id
+    @GeneratedValue
+    private long id;
     private String name;
     private double quantityGrams;
 
+    public Ingredient() {
+    }
+
     public Ingredient(String name, double quantityGrams) {
         this.name = name;
+        this.quantityGrams = quantityGrams;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantityGrams(double quantityGrams) {
         this.quantityGrams = quantityGrams;
     }
 
--- pizza-app/pizza-domain/src/main/java/ro/irian/pizzaapp/domain/Pizza.java
+++ pizza-app/pizza-domain/src/main/java/ro/irian/pizzaapp/domain/Pizza.java
@@ -1,15 +1,40 @@
 package ro.irian.pizzaapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
 
+@Entity
 public class Pizza {
+
+    @Id
+    @GeneratedValue
+    private long id;
     private String name;
     private int diameter;
+    @OneToMany
     private List<Ingredient> ingredients;
+
+    public Pizza() {
+    }
 
     public Pizza(String name, int diameter, List<Ingredient> ingredients) {
         this.name = name;
         this.diameter = diameter;
+        this.ingredients = ingredients;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
         this.ingredients = ingredients;
     }
 
